8072f03cfe0314fc76019f14ed727633bea569b2
atto08 <atto08@naver.com>
2022-07-15 17:59:00 +0900

    3days

--- test.py
+++ test.py
@@ -1,50 +1,92 @@
 import csv
 import matplotlib.pyplot as plt
-import pandas
-
+'''얘를 전역변수로 선언하지않으면 parsing함수에서 rd를 못받음.'''
+'''왜????????????????????'''
 o = open('test.csv', 'r', encoding="utf-8")
-rd = csv.DictReader(o)
+rd = csv.reader(o)
+#컬럼 별 전역변수 리스트 생성
+passengerid = []
+pclass = []
+name = []
+sex = []
+age = []
+sibsp = []
+parch = []
+ticket = []
+fare = []
+cabin = []
+embarked = []
 
-# for row in rd:
-#     print(row)
+# for list in rd:
+#     passengerid.append(list[0])
+#     pclass.append(list[1])
+#     name.append(list[2])
+#     sex.append(list[3])
+#     age.append(list[4])
+#     sibsp.append(list[5])
+#     parch.append(list[6])
+#     ticket.append(list[7])
+#     fare.append(list[8])
+#     cabin.append(list[9])
+#     embarked.append(list[10])
+# print(passengerid[1:])
 
-data = list(rd)
-print(data)
 
-def read_csv():
+
+# for i in range(1,len(age)):
+#     age1 = age[i]
+#     print(age1)
+
+# data = ()
+#
+# fig, simple_chart = plt.subplots()
+#
+# simple_chart.plot(data)
+# plt.show()
+
+# # data = list(rd)
+# # print(data)
+
+def read_csv(): #CSV읽는 함수
     o = open('test.csv', 'r', encoding="utf-8")
     rd = csv.reader(o)
+
+    for list in rd:
+        passengerid.append(list[0])
+        pclass.append(list[1])
+        name.append(list[2])
+        sex.append(list[3])
+        age.append(list[4])
+        sibsp.append(list[5])
+        parch.append(list[6])
+        ticket.append(list[7])
+        fare.append(list[8])
+        cabin.append(list[9])
+        embarked.append(list[10])
+
     return rd
+
+read_csv()
+passengerid = passengerid[1:]
+pclass = pclass[1:]
+name = name[1:]
+sex = sex[1:]
+age = age[1:]
+sibsp = sibsp[1:]
+parch = parch[1:]
+ticket = ticket[1:]
+fare = fare[1:]
+cabin = cabin[1:]
+embarked = embarked[1:]
+print(passengerid)
+
 
 def parsing(data):
     read_csv()
-    # data = list(rd)
-    # print(data)
+    data = list(rd)
+    print(data)
 
 
-#     passengerid = []
-#     pclass = []
-#     names = []
-#     sex = []
-#     age = []
-#     sibsp = []
-#     parch = []
-#     ticket = []
-#     fare = []
-#     cabin = []
-#     embarked = []
-#     passengerid.append(line[0])
-#     pclass.append(line[1])
-#     names.append(line[2])
-#     sex.append(line[3])
-#     age.append(line[4])
-#     sibsp.append(line[5])
-#     parch.append(line[6])
-#     ticket.append(line[7])
-#     fare.append(line[8])
-#     cabin.append(line[9])
-#     embarked.append(line[10])
-#
 # def write():
 #     f = open('test.csv', 'w', encoding='utf-8', newline='')
 #     wt = csv.writer(f)
@@ -54,41 +96,3 @@ def parsing(data):
 # labels = ['PassengerId','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked']
 # print(labels[0])
 # for line in rd:
-#     passengerId = []
-#     passengerId.append(line[0])
-#     print(passengerId)
-    # passengerid = []
-    # pclass = []
-    # names = []
-    # sex = []
-    # age = []
-    # sibsp = []
-    # parch = []
-    # ticket = []
-    # fare = []
-    # cabin = []
-    # embarked = []
-    # dic = {
-    #     'PassengerId': passengerid,
-    #     'pclass': pclass,
-    #     'name': names,
-    #     'sex': sex,
-    #     'age': age,
-    #     'sibsp': sibsp,
-    #     'parch': parch,
-    #     'ticket': ticket,
-    #     'fare': fare,
-    #     'cabin': cabin,
-    #     'embarked': embarked
-    # }
-    # passengerid.append(line[0])
-    # pclass.append(line[1])
-    # names.append(line[2])
-    # sex.append(line[3])
-    # age.append(line[4])
-    # sibsp.append(line[5])
-    # parch.append(line[6])
-    # ticket.append(line[7])
-    # fare.append(line[8])
-    # cabin.append(line[9])
-    # embarked.append(line[10])
--- test2.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-with open('C:/Users/user/Desktop/sparta/Titanic_tutorial/csv_savefile.csv', 'w', newline='') as f :
-    writer = csv.writer(f)
-
-    writer.writerow()
