cdd408640aad040af2153442e335e874a8a3933f
atto08 <atto08@naver.com>
2022-07-17 17:38:10 +0900

    성별 그래프 나타내기

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 '''얘를 전역변수로 선언하지않으면 parsing함수에서 rd를 못받음.'''
 '''왜????????????????????'''
 o = open('test.csv', 'r', encoding="utf-8")
@@ -17,35 +18,6 @@ fare = []
 cabin = []
 embarked = []
 
-# for list in rd:
-#     passengerid.append(list[0])
-#     pclass.append(list[1])
-#     name.append(list[2])
-#     sex.append(list[3])
-#     age.append(list[4])
-#     sibsp.append(list[5])
-#     parch.append(list[6])
-#     ticket.append(list[7])
-#     fare.append(list[8])
-#     cabin.append(list[9])
-#     embarked.append(list[10])
-# print(passengerid[1:])
-
-
-
-# for i in range(1,len(age)):
-#     age1 = age[i]
-#     print(age1)
-
-# data = ()
-#
-# fig, simple_chart = plt.subplots()
-#
-# simple_chart.plot(data)
-# plt.show()
-
-# # data = list(rd)
-# # print(data)
 
 def read_csv(): #CSV읽는 함수
     o = open('test.csv', 'r', encoding="utf-8")
@@ -78,7 +50,22 @@ ticket = ticket[1:]
 fare = fare[1:]
 cabin = cabin[1:]
 embarked = embarked[1:]
-print(passengerid)
+
+#sex(성별) 그래프 //막대
+man = sex.count('male')
+women = sex.count('female')
+
+x = np.arange(2)
+values = [man,women]
+kinds = ['male','female']
+plt.bar(x,values,color='b')
+plt.xticks(x,kinds)
+plt.show()
+
+# for i in range(len(passengerid)):
+#     print(passengerid[i])
+
+
 
 
 def parsing(data):
@@ -93,6 +80,3 @@ def parsing(data):
 #     wt.writerows([i])
 #     print()
 
-# labels = ['PassengerId','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked']
-# print(labels[0])
-# for line in rd:
