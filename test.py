69e1954e70cc531fd9a099decec4499fa900519d
atto08 <atto08@naver.com>
2022-07-18 17:38:50 +0900

    나이 str to int 형으로 변환 밑 출력 ~ING

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from numpy import int64 , float64
 '''얘를 전역변수로 선언하지않으면 parsing함수에서 rd를 못받음.'''
 '''왜????????????????????'''
 o = open('test.csv', 'r', encoding="utf-8")
 rd = csv.reader(o)
+
 #컬럼 별 전역변수 리스트 생성
 passengerid = []
 pclass = []
@@ -18,11 +21,12 @@ fare = []
 cabin = []
 embarked = []
 
-
 def read_csv(): #CSV읽는 함수
     o = open('test.csv', 'r', encoding="utf-8")
     rd = csv.reader(o)
-
+    # rd = pd.read_csv("test.csv",
+    #                  dtype={"PassengerId": int64, "Pclass": int64, "Name": object, "Sex": object, "Age": float64,"Sibsp": int64,
+    #                         "Parch": int64, "Ticket": object, "Fare": float64, "Cabin": object, "Embarked": object})
     for list in rd:
         passengerid.append(list[0])
         pclass.append(list[1])
@@ -51,29 +55,45 @@ fare = fare[1:]
 cabin = cabin[1:]
 embarked = embarked[1:]
 
+age = [float(i) for i in age]
+print(age)
+
+
 #sex(성별) 그래프 //막대
-man = sex.count('male')
-women = sex.count('female')
+# man = sex.count('male')
+# women = sex.count('female')
+#
+# sex_x = np.arange(2)
+# sex_val = [man,women]
+# sex_kinds = ['male','female']
+# sex_colors = ['b','r']
+#
+# plt.bar(sex_x,sex_val,color=sex_colors,width=0.3)
+# plt.xticks(sex_x,sex_kinds)
+# plt.show()
 
-sex_x = np.arange(2)
-sex_val = [man,women]
-sex_kinds = ['male','female']
-sex_colors = ['b','r']
 
-plt.bar(sex_x,sex_val,color=sex_colors,width=0.3)
-plt.xticks(sex_x,sex_kinds)
-plt.show()
+#age(나이) 연령별 분포도//산점
 
+# teenage = [ n for n in int(age) if n <= 11]
+# twenty
+# thirty
+# fourty
+# fifty
+# sixty
+# seventy
+# eighty
+# ninety
+# hundred
 # for i in range(len(passengerid)):
 #     print(passengerid[i])
 
 
-
-
-def parsing(data):
-    read_csv()
-    data = list(rd)
-    print(data)
+#
+# def parsing(data):
+#     read_csv()
+#     data = list(rd)
+#     print(data)
 
 
 # def write():
