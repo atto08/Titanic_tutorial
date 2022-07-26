57e6794f8c384bb2e29d8159c216ab7110e1762f
Lee <tkdduq2346@gmail.com>
2022-07-26 17:17:25 +0900

    csv 읽고, 쪼개서 종류별로 저장, 그리고 쓰고의 세가지 함수 작성

--- test_lee.py/main.py
+++ test_lee.py/main.py
@@ -34,7 +34,74 @@ def save_csv(data2):
 
             f.close()
 
+def Pclass_gr():
+    o = open("Pclass.csv", "r", encoding="utf-8")
+    rd = csv.reader(o)
+    Pclass = []
+    for i in rd:
+        for j in i:
+            Pclass.append(j)
+    Pclass.pop(0)
+
+    P1=Pclass.count("1")
+    P2=Pclass.count("2")
+    P3=Pclass.count("3")
+
+
+def Sex_gr():
+    o = open("Sex.csv", "r", encoding="utf-8")
+    rd = csv.reader(o)
+    Sex = []
+    for i in rd:
+        for j in i:
+            Sex.append(j)
+    Sex.pop(0)
+
+    S1=Sex.count("male")
+    S2=Sex.count("female")
+
+
+
+def Age_gr():
+    o = open("Age.csv", "r", encoding="utf-8")
+    rd = csv.reader(o)
+    Age = []
+    Age_int = []
+    for i in rd:
+        for j in i:
+            Age.append(j)
+
+    Age.pop(0)
+
+def Age_gr_i_f(Ag):
+
+
+
+
+    print(Age)
+
+def Embarked_gr():
+    o = open("Embarked.csv", "r", encoding="utf-8")
+    rd = csv.reader(o)
+    Embarked = []
+    for i in rd:
+        for j in i:
+            Embarked.append(j)
+    Embarked.pop(0)
+
+    E1=Embarked.count("C")
+    E2=Embarked.count("S")
+    E3=Embarked.count("Q")
+
+
+
 if __name__ == "__main__":
-    rap = read_csv()
-    rap2 = parsing(rap)
-    save_csv(rap2)
+    #rap = read_csv()
+    #rap2 = parsing(rap)
+    #save_csv(rap2)
+    Pclass_gr()
+    Sex_gr()
+    Age_gr()
+    a_i_f=age_gr()
+    Age_gr_i_f(a_i_f)
+    Embarked_gr()
