19e21fb34936772f7421169b5e1e604367430843
Lee <tkdduq2346@gmail.com>
2022-08-04 18:10:45 +0900

    csv file connect want to for csv&  survived file data

--- Train_Lee/main.py
+++ Train_Lee/main.py
@@ -15,7 +15,7 @@ def parsing(data):
     for line in data:
         file1.append(line)
     for i in range(0,12):
-        for j in range(0,891):
+        for j in range(0,892):
             file2[i].append(file1[j][i])
 
     return file2
@@ -29,77 +29,97 @@ def save_csv(data2):
         with open(name[str(n)]+".csv", "w", newline="") as f:
             writer = csv.writer(f)
 
-            for i in range(0,891):
+            for i in range(0,892):
                 writer.writerows([[data2[n][i]]])
 
             f.close()
 
+def csv_connect1(file_name1):
+    o1 = open(file_name1, "r", encoding="utf-8")
+    file1 = csv.reader(o1)
 
-
-def data_zip(name):
-    o = open(name + ".csv", "r", encoding="utf-8")
-    rd = csv.reader(o)
-    name = []
-    for i in rd:
+    csv_file=[]
+    for i in file1:
         for j in i:
-            name.append(j)
-    name.pop(0)
+            csv_file.append(j)
+    csv_file.pop(0)
 
-    return name
+    return csv_file
 
-def Age_gr(label):
-    Age_int = []
-    for i in label:
-        if i == "":
-            label.append(-1)
-        elif i is not float:
-            Age_int.append(float(i))
+def csv_connect2():
+    o2 = open("Survived.csv", "r", encoding="utf-8")
+    file2 = csv.reader(o2)
 
-    Age_group = [[], [], [], [], [], [], [], [], [], []]
+    survive_file = []
+    for i in file2:
+        for j in i:
+            survive_file.append(j)
+    survive_file.pop(0)
 
-    for j in Age_int:
-        if j >= 0 and j < 10:
-            Age_group[0].append(j)
-        elif j >= 10 and j < 20:
-            Age_group[1].append(j)
-        elif j >= 20 and j < 30:
-            Age_group[2].append(j)
-        elif j >= 30 and j < 40:
-            Age_group[3].append(j)
-        elif j >= 40 and j < 50:
-            Age_group[4].append(j)
-        elif j >= 50 and j < 60:
-            Age_group[5].append(j)
-        elif j >= 60 and j < 70:
-            Age_group[6].append(j)
-        elif j >= 70 and j < 80:
-            Age_group[7].append(j)
-        elif j >= 80 and j < 90:
-            Age_group[8].append(j)
-        elif j >= 90 and j < 100:
-            Age_group[9].append(j)
-
-def Cabin_gr(suv,cab):
-    suvived={"O": 1, "X": 0}
-    cabtin=["A","B","C","D","E","F",""]
-
-    for i in suv:
-        for j in cab:
-            if j[0]
+    return survive_file
 
 
+def Cabin_data(fn1,fn2):
+    space=[]
+    a=[]
+    b=[]
+    c=[]
+    d=[]
+    e=[]
+    f=[]
+    g=[]
+    for i in range(0,891):
+        if fn1[i] == "":
+            if fn2[i] == "0":
+                space.append(0)
+            else:
+                space.append(1)
 
-if __name__ == "__main__":
-    Age=data_zip("Age")
-    Cabin=data_zip("Cabin")
-    Embarked=data_zip("Embarked")
-    Fare=data_zip("Fare")
-    Name=data_zip("Name")
-    Parcch=data_zip("Parch")
-    Passenger=data_zip("PassengerId")
-    Pclass=data_zip("Pclass")
-    Sex=data_zip("Sex")
-    Sibsp=data_zip("Sibsp")
-    Survived=data_zip("Survived")
-    Ticket=data_zip("Ticket")
-    Age_gr(Age)
+    while "" in fn1:
+        fn1.remove("")#"" 제외한 fn1 데이터 개수:204
+
+    for i in range(0,204):
+        if fn1[i][0] == "A":
+            if fn2[i] == "0":
+                a.append(0)
+            else:
+                a.append(1)
+        elif fn1[i][0] == "B":
+            if fn2[i] == "0":
+                b.append(0)
+            else:
+                b.append(1)
+        elif fn1[i][0] == "C":
+            if fn2[i] == "0":
+                c.append(0)
+            else:
+                c.append(1)
+        elif fn1[i][0] == "D":
+            if fn2[i] == "0":
+                d.append(0)
+            else:
+                d.append(1)
+        elif fn1[i][0] == "E":
+            if fn2[i] == "0":
+                e.append(0)
+            else:
+                e.append(1)
+        elif fn1[i][0] == "F":
+            if fn2[i] == "0":
+                f.append(0)
+            else:
+                f.append(1)
+        elif fn1[i][0] == "G":
+            if fn2[i] == "0":
+                g.append(0)
+            else:
+                g.append(1)
+
+
+if  __name__ == "__main__":
+    #rap=read_csv()
+    #rap2=parsing(rap)
+    #save_csv(rap2)
+    d1=csv_connect1("Cabin.csv")
+    d2=csv_connect2()
+    Cabin_data(d1 , d2)
