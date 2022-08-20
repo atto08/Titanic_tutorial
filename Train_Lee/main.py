2fa732e726ce572c795217c719fb47c5965d2b05
Lee <tkdduq2346@gmail.com>
2022-08-20 15:38:19 +0900

    rafactoring

--- Train_Lee/main.py
+++ Train_Lee/main.py
@@ -40,33 +40,29 @@ def csv_connect1(file_name1):
     o1 = open(file_name1, "r", encoding="utf-8")
     file1 = csv.reader(o1)
 
+    o2 = open("Survived.csv", "r", encoding="utf-8")
+    file2 = csv.reader(o2)
+
     csv_file=[]
     for i in file1:
         for j in i:
             csv_file.append(j)
     csv_file.pop(0)
 
-    return csv_file
-
-def csv_connect2():
-    o2 = open("Survived.csv", "r", encoding="utf-8")
-    file2 = csv.reader(o2)
 
     survive_file = []
-
     for i in file2:
         for j in i:
             survive_file.append(j)
     survive_file.pop(0)
 
     sv_file = []
-
     for i in survive_file:
         for j in i:
             if j is not int:
                 sv_file.append(int(j))
 
-    return sv_file
+    return csv_file,sv_file
 
 def count_data(datafile):
     count=[]
@@ -77,45 +73,48 @@ def count_data(datafile):
     return count
 
 def Cabin_data(fn1,fn2,cd):
-    c_file=[[],[],[],[],[],[],[],[]] #none~g
-    c_dict={0:"n", 1:"A", 2:"B",3:"C",4:"D",5:"E",6:"F",7:"G"}
+    cabin_list=[[],[],[],[],[],[],[],[]] #none~g
+    c_dict={0:"A", 1:"B", 2:"C",3:"D",4:"E",5:"F",6:"G",7:"n"}
+    cabin_list_count = len(cabin_list)
 
     for k in range(len(cd)):
         if fn1[k] == "":
             fn1[k] = "none"
-        for j in range(0,8):
+        for j in range(cabin_list_count):
             if fn1[k][0] == c_dict[j]:
                 if fn2[k] == 0:
-                    c_file[j].append(0)
+                    cabin_list[j].append(0)
                 else:
-                    c_file[j].append(1)
+                    cabin_list[j].append(1)
 
-    return c_file
+    return cabin_list
 
 def age_data(afn1,afn2,cd):
     a_file=[[],[],[],[],[],[],[],[],[],[]] #none~90
 
-    Age_int = []
-
-    for i in afn1:
-        if i is not float:
-            Age_int.append(float(i))
+    Age_float = []
 
     for i in range(len(cd)):
         if afn1[i]=="":
             afn1[i]=-1
+
+    for i in afn1:
+        if i is not float:
+            Age_float.append(float(i))
+
+    for i in range(len(cd)):
         for j in range(0,81,10):
             k = j + 10
             n = j / 10
             m = int(n)
-
-            if j <= Age_int[i] < k:
+            #딕셔너리 사용 해서 완료 해보기
+            if j <= Age_float[i] < k:
                 if afn2[i] == 0:
                     a_file[m].append(0)
                 else:
                     a_file[m].append(1)
 
-        if Age_int[i] == -1:
+        if Age_float[i] == -1:
             if afn2[i] == 0:
                 a_file[9].append(0)
             else:
@@ -124,42 +123,43 @@ def age_data(afn1,afn2,cd):
     return a_file
 
 def embarked_data(efn1,efn2,cd):
-    e_file = [[], [], [], []]  # C, Q, S, none
+    embarked_list = [[], [], [], []]  # C, Q, S, none
     e_dict = {0: "C", 1: "Q", 2: "S"}
-
+    embarked_list_count = len(embarked_list) - 1
 
     for i in range(len(cd)):
         if efn1[i] == "":
             if efn2[i] ==0:
-                e_file[3].append(0)
+                embarked_list[-1].append(0)
             else:
-                e_file[3].append(0)
-        for j in range(0, 3):
+                embarked_list[-1].append(0)
+        for j in range(embarked_list_count):
             if efn1[i] == e_dict[j]:
                 if efn2[i] == 0:
-                    e_file[j].append(0)
+                    embarked_list[j].append(0)
                 else:
-                    e_file[j].append(1)
+                    embarked_list[j].append(1)
 
-    return e_file
+    return embarked_list
 
 def parch_data(pfn1,pfn2,cd):
     #max(pfn1) #6
-    p_file=[[],[],[],[],[],[],[]] #0~6
+    parch_list = [[],[],[],[],[],[],[]] #0~6
     p_int=[]
+    parch_list_count = len(parch_list)
 
 
     for i in range(len(cd)):
         if pfn1[i] is not int:
             p_int.append(int(pfn1[i]))
-        for j in range(0,7):
+        for j in range(parch_list_count):
             if p_int[i] == j:
                 if pfn2[i] == 0:
-                    p_file[j].append(0)
+                    parch_list[j].append(0)
                 else:
-                    p_file[j].append(1)
+                    parch_list[j].append(1)
 
-    return p_file
+    return parch_list
 
 def fare_data(ffn1,ffn2,cd):
     f_file=[[],[],[],[],[],[]] #0~50,50이상
@@ -191,66 +191,64 @@ def fare_data(ffn1,ffn2,cd):
 
 
 def pclass_data(ppfn1,ppfn2,cd):
-    p_file=[[],[],[]] #1,2,3
-    p_dict={0:1,1:2,2:3}
+    pclass_list=[[],[],[]] #1,2,3
+    p_dict={0:1, 1:2, 2:3}
     pclass_int=[]
 
     for i in ppfn1:
         if i is not int:
             pclass_int.append(int(i))
 
-
     for i in range(len(cd)):
         for j in range(0,3):
             if pclass_int[i] == p_dict[j]:
                 if ppfn2[i] == 0:
-                    p_file[j].append(0)
+                    pclass_list[j].append(0)
                 else:
-                    p_file[j].append(1)
+                    pclass_list[j].append(1)
 
-    return p_file
+    return pclass_list
 
 def sex_data(sfn1,sfn2,cd):
-    sex_file=[[],[]] #male, female
+    sex_list=[[],[]] #male, female
 
     for i in range(len(cd)):
         if sfn1[i]=="male":
             if sfn2[i]==0:
-                sex_file[0].append(0)
+                sex_list[0].append(0)
             else:
-                sex_file[0].append(1)
+                sex_list[0].append(1)
         else:
             if sfn2[i]==0:
-                sex_file[1].append(0)
+                sex_list[1].append(0)
             else:
-                sex_file[1].append(1)
+                sex_list[1].append(1)
 
-    return sex_file
+    return sex_list
 
 
 def sibsp_data(ssfn1,ssfn2,cd):
     #max(ssfn1) #max8
-    s_file=[[],[],[],[],[],[],[],[],[]] #0~8
-
+    sibsp_list=[[],[],[],[],[],[],[],[],[]] #0~8
+    sibsp_list_count = len(sibsp_list)
     sibsp_int=[]
     for i in ssfn1:
         if i is not int:
             sibsp_int.append(int(i))
 
     for i in range(len(cd)):
-        for j in range(0,9):
+        for j in range(sibsp_list_count):
             if sibsp_int[i] == j:
                 if ssfn2[i] == 0:
-                    s_file[j].append(0)
+                    sibsp_list[j].append(0)
                 else:
-                    s_file[j].append(1)
+                    sibsp_list[j].append(1)
 
-    return s_file
-
+    return sibsp_list
 
 def make_gr(dn,cn):
     x = np.arange(len(dn))  # x값 개수
-    data_name = {"Cabin":["NONE","A", "B", "C", "D", "E", "F", "G"],"Age":["0-10","10-20","20-30","30-40","40-50","50-60","60-70","70-80","80-90","NONE"],"Embarked":["C", "Q", "S","none"],
+    data_name = {"Cabin":["A", "B", "C", "D", "E", "F", "G","NONE"],"Age":["0-10","10-20","20-30","30-40","40-50","50-60","60-70","70-80","80-90","NONE"],"Embarked":["C", "Q", "S","none"],
                  "Parch":["0","1","2","3","4","5","6"],"Fare":["0-10","11-20","21-30","31-40","41-50","over_50"],"Pclass":["1","2","3"],"Sex":["male","female"],"SibSp":["0","1","2","3","4","5","6","7","8"]}
 
     xdata = data_name[cn]
@@ -293,15 +291,14 @@ if  __name__ == "__main__":
     #rap=read_csv()
     #rap2=parsing(rap)
     #save_csv(rap2)
-    d1=csv_connect1("Cabin.csv")
-    d2=csv_connect2()
+    d1,d2=csv_connect1("Age.csv")
     d3=count_data(d1)
-    cabin=Cabin_data(d1,d2,d3) #cabin 데이터
-    #age=age_data(d1,d2,d3) #age 데이터
+    #cabin=Cabin_data(d1,d2,d3) #cabin 데이터
+    age=age_data(d1,d2,d3) #age 데이터
     #embarked=embarked_data(d1,d2,d3) #embarked 데이터
     #parch=parch_data(d1,d2,d3) #parch 데이터
     #fare=fare_data(d1,d2,d3) #fare 데이터
     #pclass=pclass_data(d1,d2,d3) #pclass 데이터
     #sex=sex_data(d1,d2,d3) #sex 데이터
     #sibsp=sibsp_data(d1,d2,d3) #sibsp 데이터
-    make_gr(cabin,"Cabin")
+    make_gr(age,"Age")
